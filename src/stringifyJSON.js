aa710889825aa16355eb753284bc6e4cbe95670c
mlee0330 <michaelyurilee@gmail.com>
2015-09-10 14:16:11 -0700

    Implemented stringify to pass all tests.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,25 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (Array.isArray(obj)) {
+    obj = obj.map(function(item) {
+      return stringifyJSON(item);
+    });
+    return '[' + obj + ']';
+  }
+  if (obj && typeof obj === 'object') {
+    var stringified = [];
+    for (var x in obj) {
+      if (obj[x] === undefined || typeof obj[x] === 'function') {
+        continue;
+      }
+      stringified.push(stringifyJSON(x) + ":" + stringifyJSON(obj[x]));
+    }
+    return '{' + stringified.join() + '}';
+  }
+  if (typeof obj === 'string') {
+    return  '"' + obj + '"';
+  }
+  return '' + obj;
 };
+
